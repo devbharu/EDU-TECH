f56b1766c6d03593b7cce7ddccf6262d3f5970d1
Bharath s <devbharu04@gmail.com>
2025-04-09 21:51:11 +0530

    tailwind and frontend

---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      hi there
+      <div className='bg-blue-200'>
+        hi there
+      </div>
+    </>
+  )
+}
+
+export default App
